c63a1869ea62f42cbc06183d94f021930e5b3068
Diego Soares <soares_diego666@hotmail.com>
2023-10-28 16:05:50 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Nokia: ");
+Nokia n1 = new("600390334", "9384759", "Nokia3310", 64);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Xwitter");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testando Iphone: ");
+Iphone iph = new("555097999", "23749768", "Iphone2", 16);
+iph.Ligar();
+iph.ReceberLigacao();
+iph.InstalarAplicativo("Bluesky");
